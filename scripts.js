deae5cbb847cf82e6fae68d6b8ebf00fe3a7f65d
izzy <mista.rol@gmail.com>
2023-09-30 04:20:31 +0700

    add fonts, styling, delay, etc

--- scripts.js
+++ scripts.js
@@ -1,5 +1,17 @@
-const intervalDuration = 5000;
-let indices = [0, 0, 0, 0]; // Indices for each array
+const intervalDuration = 5000; // Adjust this for headline change interval
+const transitionDuration = 500; // Adjust this for the duration of the unmounting
+const delayBetweenHeadlines = 100; // Adjust this for delay between headlines appearing
+let shownIndices = [[], [], [], []]; // Store indices that have been shown
+
+function getUniqueRandomIndex(array, arrayIndex) {
+    if (shownIndices[arrayIndex].length >= array.length) shownIndices[arrayIndex] = [];
+    let index;
+    do {
+        index = Math.floor(Math.random() * array.length);
+    } while (shownIndices[arrayIndex].includes(index));
+    shownIndices[arrayIndex].push(index);
+    return index;
+}
 
 function displayHeadline(columnId, array, arrayIndex) {
     const col = document.getElementById(columnId);
@@ -7,37 +19,30 @@ function displayHeadline(columnId, array, arrayIndex) {
         console.error(`Element with ID ${columnId} not found!`);
         return;
     }
-    if(!Array.isArray(array) || array.length === 0) {
-        console.error(`Array for ${columnId} is not defined or empty!`);
-        return;
-    }
-    const index = indices[arrayIndex];
-    if(index >= array.length) {
-        console.error(`Index ${index} out of bounds for ${columnId}!`);
-        return;
-    }
-    const headline = array[index];
-    if(!headline) {
-        console.error(`Headline at index ${index} for ${columnId} is undefined!`);
-        return;
-    }
-    console.log(`Displaying headline for ${columnId} - ${headline}`);
-    col.textContent = headline;
-    indices[arrayIndex] = (index + 1) % array.length;
+    setTimeout(() => {
+        const index = getUniqueRandomIndex(array, arrayIndex);
+        const headline = array[index];
+        col.textContent = headline;
+        console.log(`Displaying headline for ${columnId} - ${headline}`);
+    }, transitionDuration + (arrayIndex * delayBetweenHeadlines));
 }
 
 function displayHeadlines() {
-    console.log('Displaying headlines...');
-    displayHeadline('headlineOne', arrayOne, 0);
-    displayHeadline('headlineTwo', arrayTwo, 1);
-    displayHeadline('headlineThree', arrayThree, 2);
-    displayHeadline('headlineFour', arrayFour, 3);
+    console.log('Unmounting text...');
+    document.querySelectorAll('.column').forEach(col => col.textContent = '');
+    setTimeout(() => {
+        console.log('Displaying headlines...');
+        displayHeadline('headlineOne', arrayOne, 0);
+        displayHeadline('headlineTwo', arrayTwo, 1);
+        displayHeadline('headlineThree', arrayThree, 2);
+        displayHeadline('headlineFour', arrayFour, 3);
+    }, transitionDuration);
 }
 
 setInterval(() => {
     console.log('Interval triggered...');
     displayHeadlines();
-}, intervalDuration);
+}, intervalDuration + transitionDuration);
 
 // Kick off the first display
 console.log('Kicking off the first display...');
